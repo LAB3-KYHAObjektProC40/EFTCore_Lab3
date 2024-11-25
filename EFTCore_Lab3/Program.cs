01c8e7e160e10dec307b399fc2faf409f1ae6bdd
Simon Major <simon.major@learnet.se>
2024-11-26 00:33:53 +0100

    Commented out instert, now we add read function

--- EFTCore_Lab3/Program.cs
+++ EFTCore_Lab3/Program.cs
@@ -1,9 +1,11 @@
 ﻿using EFCore.Models;
 using EFTCore_Lab3.Models;
+using System.Collections.Generic;
 
 
 Console.WriteLine("Hello, World!");
-insertWeatherData();
+//insertWeatherData();
+readWeatherData();
 
 Console.WriteLine("Press any key to continue");
 Console.ReadKey();
@@ -21,6 +23,19 @@ static void insertWeatherData()
         db.Add(weatherdata);
 
         db.SaveChanges();
+    }
+    return;
+}
+
+static void readWeatherData()
+{
+    using(var db = new EFContext())
+    {
+        List<WeatherData> weatherdata = db.WeatherData.ToList();
+        foreach (WeatherData p in weatherdata)
+        {
+            Console.WriteLine("{0} {1}", p.Id, p.Location);
+        }
     }
     return;
 }
