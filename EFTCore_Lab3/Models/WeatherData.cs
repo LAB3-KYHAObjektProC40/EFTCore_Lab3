8d4aeac416fee33c2ff1a32136cbea00d7b5aec2
Simon Major <simon.major@learnet.se>
2024-11-25 23:53:47 +0100

    Add ID..... Primary key as error with "Unable to create a 'DbContext' of type 'EFContext'." 'WeatherData' requires a primary key to be defined.

--- EFTCore_Lab3/Models/WeatherData.cs
+++ EFTCore_Lab3/Models/WeatherData.cs
@@ -6,13 +6,14 @@ namespace EFTCore_Lab3.Models
 {
     public class WeatherData
     {
-        public DateTime Timestamp { get; set; }
-        
-        public double MyProperty { get; set; }
-        
-        public int Humidity { get; set; }
+        public int Id { get; set; }  // Primary key
+        public DateTime Timestamp { get; set; }   // Date and time of the measurement
 
-        public string Location { get; set; }
+        public double Temperature { get; set; }  // Temperature in Celsius
+
+        public int Humidity { get; set; } // Humidity in percentage
+
+        public string Location { get; set; }  // Measurement location (e.g., "Ute", "Inne")
 
 
     }
