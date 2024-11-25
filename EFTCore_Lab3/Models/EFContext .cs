7a389a721eddc03e6e0cb53f6b38266e52bd7080
Simon Major <simon.major@learnet.se>
2024-11-25 23:36:24 +0100

    DbContext with DBSet + tellign to use SQL server

---
+++ EFTCore_Lab3/Models/EFContext .cs
@@ -0,0 +1,17 @@
+﻿
+using EFTCore_Lab3.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCore.Models;
+
+public class EFContext : DbContext
+
+{
+ private const string connectionString =
+"Server=(localdb)\\MSSQLLocalDB;Database=EFCore;Trusted_Connection=True;";
+protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+{
+    optionsBuilder.UseSqlServer(connectionString);
+}
+    public DbSet<WeatherData> WeatherData { get; set; }
+}
