f9c3e50741d38195580a909a19832dc06affc873
Simon Major <simon.major@learnet.se>
2024-11-25 23:14:13 +0100

    Add project files.

---
+++ EFTCore_Lab3/Models/WeatherData.cs
@@ -0,0 +1,19 @@
+﻿
+
+using System.Numerics;
+
+namespace EFTCore_Lab3.Models
+{
+    public class WeatherData
+    {
+        public DateTime Timestamp { get; set; }
+        
+        public double MyProperty { get; set; }
+        
+        public int Humidity { get; set; }
+
+        public string Location { get; set; }
+
+
+    }
+}
