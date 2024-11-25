bfb2b3dde714256bf259c80851c09c40f4410894
Simon Major <simon.major@learnet.se>
2024-11-26 00:24:59 +0100

    Adding simpel data - Ute or Inne to Location

--- EFTCore_Lab3/Program.cs
+++ EFTCore_Lab3/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using EFCore.Models;
+using EFTCore_Lab3.Models;
+
+
 Console.WriteLine("Hello, World!");
+insertWeatherData();
+
+Console.WriteLine("Press any key to continue");
+Console.ReadKey();
+
+static void insertWeatherData()
+{
+    using(var db = new EFContext())
+    {
+        WeatherData weatherdata = new WeatherData();
+        weatherdata.Location = "Ute";
+        db.Add(weatherdata);
+
+        weatherdata = new WeatherData();
+        weatherdata.Location = "Inne";
+        db.Add(weatherdata);
+
+        db.SaveChanges();
+    }
+    return;
+}
